998645eb5e8523085ecbce56aac391b65d00bca4
RODOLFO-SOLANO\Esteban <rsolano1996@gmail.com>
2017-10-24 14:44:22 -0600

    restful

---
+++ RESTFUL/RESTAPI/RESTAPI/Controllers/PostgreController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Npgsql;
+using System.Data.SqlClient;
+
+namespace RESTAPI.Controllers
+{
+    public class PostgreController : ApiController
+    {
+        string connectionString = "Server=postgreserver-20170401.postgres.database.azure.com; " +
+        "Port=5432; Database=gasstationpharmacy; User Id=rsolano10@postgreserver-20170401; Password=BasesDatosak7;";
+
+        [HttpGet]
+        public string getAll()
+        {
+            using (NpgsqlConnection conn = new NpgsqlConnection(connectionString))
+            {
+                /*// Connect to the database
+                conn.Open();
+
+                // Read rows
+                NpgsqlCommand selectCommand = new NpgsqlCommand("SELECT * FROM MyTable", conn);
+                NpgsqlDataReader results = selectCommand.ExecuteReader();
+
+                // Enumerate over the rows
+                while (results.Read())
+                {
+                    Console.WriteLine("Column 0: {0} Column 1: {1}", results[0], results[1]);
+                   
+                }*/
+                return "ok";
+            }
+        }
+    }
+}
---
+++ RESTFUL/RESTAPI/RESTAPI/JSONSerializer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Diagnostics;
+
+namespace RESTAPI
+{
+    public class JSONSerializer
+    {
+        public IEnumerable<Dictionary<string, object>> Serialize(SqlDataReader reader)
+        {
+            var results = new List<Dictionary<string, object>>();
+            var cols = new List<string>();
+            for (var i = 0; i < reader.FieldCount; i++)
+                cols.Add(reader.GetName(i));
+
+            while (reader.Read())
+            {
+
+                results.Add(SerializeRow(cols, reader));
+            }
+            return results;
+        }
+        public Dictionary<string, object> singleserialize(SqlDataReader reader)
+        {
+            var results = new List<Dictionary<string, object>>();
+            var cols = new List<string>();
+            for (var i = 0; i < reader.FieldCount; i++)
+            {
+                cols.Add(reader.GetName(i));
+            }
+            reader.Read();
+            return SerializeRow(cols, reader);
+        }
+        private Dictionary<string, object> SerializeRow(IEnumerable<string> cols,
+                                                        SqlDataReader reader)
+        {
+            var result = new Dictionary<string, object>();
+            foreach (var col in cols)
+            {
+                result.Add(col, reader[col]);
+            }
+            return result;
+        }
+    }
+}

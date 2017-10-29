f6ed4d5d9e98be16bd462e8368fe098de9f068e3
adrian039 <adrian_sa12@outlook.com>
2017-10-29 11:31:02 -0600

    borrado de rest

--- RESTFUL/RESTFUL/RESTFUL/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace RESTFUL.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- RESTFUL/RESTFUL/RESTFUL/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace RESTFUL.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}

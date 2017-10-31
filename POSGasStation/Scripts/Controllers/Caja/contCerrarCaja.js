ab7f59192293effd82e421bfc3c14f7d36444b8a
marcf98 <32026019+marcf98@users.noreply.github.com>
2017-10-30 22:49:03 -0600

    cerrar caja
    se crea la vista cerrar caja

---
+++ POSGasStation/Scripts/Controllers/Caja/contCerrarCaja.js
@@ -0,0 +1,15 @@
+angular.module("mainModule").controller("contCerrarCaja", ["$scope","$http","userService",'$location',
+function($scope,$http,userService,$location) {
+  $scope.idCash = "";
+  $scope.amount="";
+
+  $scope.openCash=function(){
+    alert("Hola");
+  };
+
+      $scope.init = function(){
+      alert("hola");
+    };
+
+
+}]);
--- POSGasStation/Scripts/Controllers/Caja/contRegVenta.js
+++ POSGasStation/Scripts/Controllers/Caja/contRegVenta.js
@@ -1,6 +1,8 @@
 angular.module("mainModule").controller("contRegVenta", ["$scope","$http","userService",'$location',
 function($scope,$http,userService,$location) {
   $scope.idClient = "";
+  $scope.ean="";
+  $scope.qty="";
 
   $scope.openCash=function(){
     alert("Entre");

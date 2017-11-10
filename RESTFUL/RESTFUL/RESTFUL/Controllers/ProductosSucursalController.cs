68fafeb11f9a2251a040bbf5687d1e6d3b350a4e
adrian039 <adrian_sa12@outlook.com>
2017-11-10 00:13:28 -0600

    productosController REST

---
+++ RESTFUL/RESTFUL/RESTFUL/Controllers/ProductosSucursalController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using RESTFUL.Models;
+
+namespace RESTFUL.Controllers
+{
+    public class ProductosSucursalController : ApiController
+    {
+        
+
+       [HttpGet]
+        public producto hayDisponible([FromBody]productosxsucursal prod)
+        {
+            try
+            {
+                using (gspEntity entities= new gspEntity())
+                {
+                    var resp = entities.productosxsucursals.FirstOrDefault(e => (e.idproducto == prod.idproducto) && (e.idsucursal == prod.idsucursal)
+                     && (e.cantidad >= prod.cantidad));
+                    if (resp != null)
+                    {
+                        var product = entities.productos.FirstOrDefault(e => e.ean == prod.idproducto);
+                        return  product;
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+
+            }catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+    }
+}

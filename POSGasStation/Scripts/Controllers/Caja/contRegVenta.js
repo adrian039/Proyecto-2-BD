ff7be60cc609a2ec33fc900cbf5c67ab6aa0a12d
marcf98 <32026019+marcf98@users.noreply.github.com>
2017-11-13 18:31:38 -0600

    web
    validation

--- POSGasStation/Scripts/Controllers/Caja/contRegVenta.js
+++ POSGasStation/Scripts/Controllers/Caja/contRegVenta.js
@@ -10,45 +10,54 @@ function($scope,$http,clientService,userService,$location) {
   };
 
   $scope.verifyUser=function(){
-    var url = "http://gsprest.azurewebsites.net/api/Clientes?cedula="+this.idClient;
-    $scope.getHttp(url,(data)=>{
-      if(data){
-        alert("Client:" +data.nombre+" "+data.papellido);
-        clientService.setName(data.nombre);
-        clientService.setpAp(data.papellido);
-        clientService.setsAp(data.sapellido);
-        clientService.setID(data.cedula);
-      }
-      else{
-        $('#myModal').modal({ show: false});
-        $('#myModal').modal("show");
-      }
-    })
+    if(userService.getState()){
+      var url = "http://gsprest.azurewebsites.net/api/Clientes?cedula="+this.idClient;
+      $scope.getHttp(url,(data)=>{
+        if(data){
+          alert("Client:" +data.nombre+" "+data.papellido);
+          clientService.setName(data.nombre);
+          clientService.setpAp(data.papellido);
+          clientService.setsAp(data.sapellido);
+          clientService.setID(data.cedula);
+        }
+        else{
+          $('#myModal').modal({ show: false});
+          $('#myModal').modal("show");
+        }
+      })
+    }
+    else{
+      alert("Cashier is not opent");
+    }
   };
 
   $scope.addProduct=function(){
+    if(userService.getState()){
+      var url="http://gsprest.azurewebsites.net/api/ProductosSucursal";
+      var sendData = {
+        "idproducto": parseInt(this.ean),
+        "cantidad": parseInt(this.qty),
+        "idsucursal": parseInt(userService.getSucursal())
+      };
+      $scope.postHttp(url,sendData,(data)=>{
+        if(data){
+          data["qty"]=parseInt(this.qty);
+          var productos={
+            "nombre": data.nombre,
+            "qty":parseInt(this.qty),
+            "ean":this.ean
+          };
 
-    var url="http://gsprest.azurewebsites.net/api/ProductosSucursal";
-    var sendData = {
-      "idproducto": parseInt(this.ean),
-      "cantidad": parseInt(this.qty),
-      "idsucursal": parseInt(userService.getSucursal())
-    };
-    $scope.postHttp(url,sendData,(data)=>{
-      if(data){
-        data["qty"]=parseInt(this.qty);
-        var productos={
-          "nombre": data.nombre,
-          "qty":parseInt(this.qty),
-          "ean":this.ean
-        };
-
-        this.prodList.push(productos);
-      }
-      else{
-        alert("The product does not exist or is not available");
-      }
-    });
+          this.prodList.push(productos);
+        }
+        else{
+          alert("The product does not exist or is not available");
+        }
+      });
+    }
+    else{
+      alert("Cashier not open");
+    }
   };
 
   $scope.generic=function(){
@@ -66,38 +75,42 @@ function($scope,$http,clientService,userService,$location) {
   }
   
   $scope.registerSale=function(){
-    var url="http://gsprest.azurewebsites.net/api/Ventas";
-    var prodVenta=[];
-    for (var i=0;i<this.prodList.length;i++){
-      var productos={
-        "ean":parseInt(this.prodList[i].ean),
-        "cantidad":this.prodList[i].qty
+    if(userService.getState()){
+      var url="http://gsprest.azurewebsites.net/api/Ventas";
+      var prodVenta=[];
+      for (var i=0;i<this.prodList.length;i++){
+        var productos={
+          "ean":parseInt(this.prodList[i].ean),
+          "cantidad":this.prodList[i].qty
+        };
+        prodVenta.push(productos);
+      }
+      var payType=this.type.split(":",1);
+      var sendData = {
+        "idCliente": clientService.getID(),
+        "idEmpleado":userService.getUser().cedula,
+        "idSucursal": parseInt(userService.getSucursal()),
+        "tipoPago": parseInt(payType),
+        "productos":prodVenta
       };
-      prodVenta.push(productos);
+      console.log(prodVenta);
+      if(prodVenta.length>0){
+        $scope.postHttp(url,sendData,(data)=>{
+          console.log("Data: "+data);
+          if(data!="null"){
+            alert("Order completed");
+          }
+          else{
+            alert("Error, check that the information is complete");
+          }
+        });
+      }else{
+        alert("You must add products");
+      }
     }
-    var payType=this.type.split(":",1);
-    var sendData = {
-      "idCliente": clientService.getID(),
-      "idEmpleado":userService.getUser().cedula,
-      "idSucursal": parseInt(userService.getSucursal()),
-      "tipoPago": parseInt(payType),
-      "productos":prodVenta
-    };
-    console.log(prodVenta);
-    if(prodVenta.length>0){
-      $scope.postHttp(url,sendData,(data)=>{
-        console.log("Data: "+data);
-        if(data!="null"){
-          alert("Order completed");
-        }
-        else{
-          alert("Error, check that the information is complete");
-        }
-      });
-    }else{
-      alert("You must add products");
+    else{
+      alert("Cashier not open");
     }
-    
 
   };
 

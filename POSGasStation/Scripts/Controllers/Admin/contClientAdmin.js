f484de26110de831f7ce070b0e45af130edb2815
marcf98 <32026019+marcf98@users.noreply.github.com>
2017-11-17 00:47:17 -0600

    Web
    PUTS

--- POSGasStation/Scripts/Controllers/Admin/contClientAdmin.js
+++ POSGasStation/Scripts/Controllers/Admin/contClientAdmin.js
@@ -2,6 +2,11 @@ angular.module("mainModule").controller("contClientAdmin",["$scope","$http",
 function($scope,$http) {
   $scope.clientList;
 
+  $scope.cedula;
+  $scope.nombre;
+  $scope.pApellido;
+  $scope.sApellido;
+  
 
       $scope.init = function(){
         var url='http://gsprest.azurewebsites.net/api/Clientes';
@@ -12,29 +17,21 @@ function($scope,$http) {
       };
 
 
-      $scope.edit=function(id,dir,nme,pAp,sAp,pass,usr,ste,pnz,date){
-
-        var url = 'http://'+getIp()+':58706/api/Clientes';
-        var data={
-          "Cedula":id,
-          "Direccion":dir,
-          "Nombre":nme,
-          "pApellido":pAp,
-          "sApellido":sAp,
-          "Password":pass,
-          "Username":usr,
-          "Estado":ste,
-          "Penalizacion":pnz,
-          "Nacimiento":date
-
+      $scope.edit=function(){
+        console.log("Cedula "+this.cedula);
+        var url = 'http://gsprest.azurewebsites.net/api/Clientes/'+this.cedula;
+        var sendData = {
+          "cedula": parseInt(this.cedula),
+          "nombre": this.nombre,
+          "papellido": this.pApellido,
+          "sapellido":this.sApellido,
+          "estado":1
         };
-        $http.put(url,data)
+        $http.put(url,sendData)
         .then(
           function(response){
               // success callback
-              console.log("erase");
-              animation();
-
+              $scope.init();
             }, 
             function(response){
               // failure callback
@@ -44,22 +41,10 @@ function($scope,$http) {
       }
 
 
-      $scope.delete=function(id,dir,nme,pAP,sAp,pass,usr,ste,pnz,data){
+      $scope.delete=function(id){
 
-        var url = 'http://'+getIp()+':58706/api/Clientes';
-        var data={
-          "Cedula":id,
-          "Direccion":dir,
-          "Nombre":nme,
-          "pApellido":pAp,
-          "sApellido":sAp,
-          "Password":pass,
-          "Username":usr,
-          "Estado":ste,
-          "Penalizacion":pnz,
-          "Nacimiento":date
-
-        };
+        var url = 'http://gsprest.azurewebsites.net/api/Clientes/'+id;
+        
         $http.delete(url,data)
         .then(
             function(response){
--- POSGasStation/Scripts/Controllers/Admin/contNuevSucAdmin.js
+++ POSGasStation/Scripts/Controllers/Admin/contNuevSucAdmin.js
@@ -2,53 +2,33 @@ angular.module("mainModule").controller("contNuevSucAdmin", ["$scope","$http","d
 'storeService','userService',
 
   function($scope,$http,directionService,$location,storeService,userService) {
-    $scope.idEmp = "";
-    $scope.name="";
-    $scope.idCant="";
-    $scope.idProv="";
-    $scope.idDist="";
-    $scope.dir="";
-    $scope.lat="";
-    $scope.lng="";
 
-    $scope.empList;
-    $scope.provList;
-    $scope.cantList;
-    $scope.distList;
-
-    $scope.idAdmin=empresaAdmin;
+    $scope.idSucursal;
+    $scope.nombre;
+    $scope.direccion;
 
 
     $scope.init = function(){
-      var url = 'http://'+getIp()+':58706/api/Distrito';
-      $http.get(url).then(function(msg){
-        $scope.distList = msg.data;
-        
-      });
-      url = 'http://'+getIp()+':58706/api/Cantones';
-      $http.get(url).then(function(msg){
-        $scope.cantList = msg.data;
-      });
-      url = 'http://'+getIp()+':58706/api/Empresa';
-      $http.get(url).then(function(msg){
-        $scope.empList = msg.data;
-      });
-      url = 'http://'+getIp()+':58706/api/Provincias';
-      $http.get(url).then(function(msg){
-        $scope.provList = msg.data;
-      });
+
 
     }
 
 
-    $scope.createStore=function(nme,dr){
-      var url='http://'+getIp()+':58706/api/Sucursal';
-      var sendData={"Nombre": nme, "detalleDireccion":dr, "idEmpresa":userService.getCompany(), "idCanton":directionService.getCity(), "idProvincia": directionService.getState(), "idDistrito":directionService.getDistrict() , "Estado":"1"};
-      console.log("send: "+sendData);
-      $scope.postHttp(url,sendData,(data)=>{
-        console.log("id: "+data.idSucursal);
-        storeService.setID(data.idSucursal);
-      })
+    $scope.createStore=function(){
+      var url = "http://gsprest.azurewebsites.net/api/Sucursales";
+      var sendData = {
+        "idsucursal": parseInt(this.idSucursal),
+        "nombre": this.nombre,
+        "direccion": this.direccion,
+        "img":globalImage,
+        "estado":1,
+        "idempresa":parseInt(userService.getCompany())
+      }
+
+     $scope.postHttp(url,sendData,(data)=>{
+        console.log("Data: "+data);
+      });
+
     }
 
   }]);

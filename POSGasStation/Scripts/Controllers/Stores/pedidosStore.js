5dbbae05e7897d490575be577505f57da02db0af
marcf98 <32026019+marcf98@users.noreply.github.com>
2017-11-21 01:16:43 -0600

    web
    order list

--- POSGasStation/Scripts/Controllers/Stores/pedidosStore.js
+++ POSGasStation/Scripts/Controllers/Stores/pedidosStore.js
@@ -1,58 +1,12 @@
 angular.module("mainModule").controller("pedidosStore",["$scope","$http", "$location","userService", "orderService",  
   function($scope,$http, $location, userService, orderService) {
-    $scope.state = "";
-    $scope.listOrders=[];
-    $scope.ProductOrders=[];
-    var pedidoselected=1;
-    $scope.statesId=[
-    {'id':'1:','Estado':'Nuevo'},
-    {'id':'2:','Estado':'Preparado'},
-    {'id':'3:','Estado':'Facturado'},
-    {'id':'4:','Estado':'Retirado'}
-    ];
-
-
-    $scope.getHttp= function(url , callback){
-      var httpObject = $http.get(url);
-      httpObject.then(function(promise){
-        callback(promise.data);
-    }, function(error){ console.log(error);})}
-
-      $scope.postHttp = function(url,data,callback){
-        var httpObject = $http.post(url,data);
-        httpObject.then(function(promise){
-          callback(promise.data);
-      }, function(error){ console.log(error);})}
+      $scope.listOrders;
 
         $scope.init = function(){
-            var url = 'http://'+getIp()+':58706/api/Pedidos?idSuc='+userService.getSucursal();
+            var url = 'http://gsprest.azurewebsites.net/api/Ventas?idsuc='+userService.getSucursal();
             $http.get(url).then(function(msg){
                 console.log(msg.data);
                 $scope.listOrders= msg.data;
             });
         };
-        $scope.initProduct = function(){
-            console.log("PEdido Selected: " + orderService.getCurrentOrder());
-            var url = 'http://'+getIp()+':58706/api/Productos?Ped='+orderService.getCurrentOrder();
-            $http.get(url).then(function(msg){
-                console.log(msg.data);
-                $scope.ProductOrders= msg.data;
-            });
-        };
-        
-        $scope.update=function(ste, pedido){
-            var id=ste.split(":",1);
-            var newid = id[0];
-            var url = 'http://'+getIp()+':58706/api/EstadosPedido';
-            var json = {"idPedido":pedido,"Estado":newid};
-            $http.post(url,json).then(function(msg){
-                console.log(msg.data);
-                $scope.init();
-            });
-        }
-        $scope.see=function(pedido){
-            orderService.setCurrentOrder(pedido);
-            $location.path("/Stores/ProductsxOrder");
-        }
-    
     }]);

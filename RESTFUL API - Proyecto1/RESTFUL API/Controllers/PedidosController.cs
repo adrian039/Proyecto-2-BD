247c63da5b995215720c5bfa0511cbbd98e656a3
adrian039 <adrian_sa12@outlook.com>
2017-11-21 21:36:08 -0600

    cambio rest 1

--- RESTFUL API - Proyecto1/RESTFUL API/Controllers/PedidosController.cs
+++ RESTFUL API - Proyecto1/RESTFUL API/Controllers/PedidosController.cs
@@ -209,14 +209,8 @@ namespace RESTFUL_API.Controllers
                      conn.Open();
                      cmd.ExecuteReader();
                      var message = Request.CreateResponse(HttpStatusCode.Created, pedido);
-                    if (pedido.Estado == 3)
-                    {
-                        return regVentaToApi(pedido.idPedido, pedido.sucursalRecojo.Value, pedido.idCliente.Value);
-                    }
-                    else
-                    {
-                        return Request.CreateResponse(HttpStatusCode.OK, pedido);
-                    }
+                    return message;
+                    
                 }
             }
             catch (Exception ex)
@@ -225,60 +219,6 @@ namespace RESTFUL_API.Controllers
             }
         }
 
-        private HttpResponseMessage regVentaToApi(int venta, int suc, int cliente)
-        {
-            try
-            {
-                using (SqlConnection conn = new SqlConnection(DatabaseConnectionString))
-                {
-                    SqlCommand cmd1 = new SqlCommand("SELECT idProducto AS ean, Cantidad AS cantidad FROM DETALLEPEDIDO WHERE idPedido=@id");
-                    cmd1.Parameters.AddWithValue("@id", venta);
-                    cmd1.Connection = conn;
-                    conn.Open();
-                    using (var reader = cmd1.ExecuteReader())
-                    {
-                       var r = serial.Serialize(reader);
-                       string JSONresult;
-                        JSONresult = JsonConvert.SerializeObject(r);
-                       string jsonText = JSONresult.Replace("\"", "");
-                        string sqlFormattedDate = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss.fff");
-                        JObject ven = new JObject();
-                        ven.Add("idCliente", cliente);
-                        ven.Add("idEmpleado", 0);
-                        ven.Add("idSucursal", suc);
-                        ven.Add("productos", JArray.Parse(JSONresult));
-                        ven.Add("tipoPago", 1);
-                        ven.Add("fecha", sqlFormattedDate);
-                        ven.Add("starts", "00:00:00");
-                        ven.Add("ends", "00:00:00");
-                        ven.Add("idcaja", 0);
-                        var httpWebRequest = (HttpWebRequest)WebRequest.Create("http://gsprest.azurewebsites.net/api/Ventas/");
-                        httpWebRequest.ContentType = "application/json";
-                        httpWebRequest.Method = "POST";
-
-                        using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
-                        {
-                            streamWriter.Write(ven);
-                            streamWriter.Flush();
-                            streamWriter.Close();
-                        }
-
-                        var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
-                       using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
-                        {
-                            var result = streamReader.ReadToEnd();
-                            return Request.CreateResponse(HttpStatusCode.OK, result.Replace("\"",""));
-                        }
-
-                    }
-
-                     
-                }
-            }catch(Exception ex)
-            {
-                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
-            }
-        }
 
         
     }
